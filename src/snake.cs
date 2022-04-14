c038b0b3c8b5053cedb12b13b8629186dfc00e04
Bike nim <63693807+ikenim@users.noreply.github.com>
2022-04-14 11:22:26 -0600

    Update snake.cs

--- src/snake.cs
+++ src/snake.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Drawing;
+using System.Windows;
 
 class Snake {
-  public static void Main (string[] args) {
-    
+  private void ImageExampleForm_Paint(object sender, PaintEventArgs e) {
+    Image newImage = Image.fromFile("segment.png");
+
+    PointF ulCorner = new PointF(100.0F, 100.0F);
+
+    e.Graphics.DrawImage(newImage, ulCorner);
   }
 }
